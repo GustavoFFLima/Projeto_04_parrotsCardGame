2711a729e8a3c0bada6172774c778c8303717ec2
GustavoFFLima <frazao39@gmail.com>
2022-10-18 21:55:54 -0300

    atualização, correções de erros

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let quantidadeCartas = Number(prompt("Com quantas cartas quer jogar, só vale em par,de 4 a 14."));
-while (quantidadeCartas < 4 ) {
+while (quantidadeCartas < 4 || quantidadeCartas > 14) {
     alert('Número inválidos, escolher outro numero! ');
-    quantidadeCartas = prompt("Com quantas cartas quer jogar, só vale em par,de 4 a 14.");
-}
-while (quantidadeCartas > 14) {
-    alert('Número inválidos, escolher outro numero! ');
-    quantidadeCartas = prompt("Com quantas cartas quer jogar, só vale em par,de 4 a 14.");
-}
+    quantidadeCartas = Number(prompt("Com quantas cartas quer jogar, só vale em par,de 4 a 14."));
+};
+
